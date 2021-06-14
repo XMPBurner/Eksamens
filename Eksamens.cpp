83aa9704366e7f22a4bd59aa066830f3d4518c1f
unknown <emilsporikis2@gmail.com>
2021-06-14 12:47:59 +0300

    jautajumu skelets ir izveidots

--- Eksamens.cpp
+++ Eksamens.cpp
@@ -1,1 +1,339 @@
 #include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	string atbilde;
+	int n=0, j=0, ;
+	
+	#1.Jautajums
+	cout<<"1.jautajums \n\n";
+	cout<<"atbildi a/b \n\n";
+	
+	#Atbilzu varianti
+	cout<<"a \n\n";
+	cout<<"b \n\n";
+	cout<<"c \n\n";
+	cout<<"d \n\n";
+	
+	#nosaka vai ir atbildets pareizi vai nepareizi
+	while(n == 0){
+		cout<<"atbilde: "; cin>>atbilde;
+	if(atbilde == "a"){
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "b") {
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "c") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else if(atbilde == "d") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else{
+		cout<<"\ndriks tikkai atbildet ar a,b,c vai d \n\n";
+		}
+	}
+	n=0;
+	
+	#2.Jautajums
+	cout<<"1.jautajums \n\n";
+	cout<<"atbildi a/b \n\n";
+	
+	#Atbilzu varianti
+	cout<<"a \n\n";
+	cout<<"b \n\n";
+	cout<<"c \n\n";
+	cout<<"d \n\n";
+	
+	#nosaka vai ir atbildets pareizi vai nepareizi
+	while(n == 0){
+		cout<<"atbilde: "; cin>>atbilde;
+	if(atbilde == "a"){
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "b") {
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "c") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else if(atbilde == "d") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else{
+		cout<<"\ndriks tikkai atbildet ar a,b,c vai d \n\n";
+		}
+	}
+	n=0;
+	
+	#3.Jautajums
+	cout<<"1.jautajums \n\n";
+	cout<<"atbildi a/b \n\n";
+	
+	#Atbilzu varianti
+	cout<<"a \n\n";
+	cout<<"b \n\n";
+	cout<<"c \n\n";
+	cout<<"d \n\n";
+	
+	#nosaka vai ir atbildets pareizi vai nepareizi
+	while(n == 0){
+		cout<<"atbilde: "; cin>>atbilde;
+	if(atbilde == "a"){
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "b") {
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "c") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else if(atbilde == "d") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else{
+		cout<<"\ndriks tikkai atbildet ar a,b,c vai d \n\n";
+		}
+	}
+	n=0;
+	
+	#4.Jautajums
+	cout<<"1.jautajums \n\n";
+	cout<<"atbildi a/b \n\n";
+	
+	#Atbilzu varianti
+	cout<<"a \n\n";
+	cout<<"b \n\n";
+	cout<<"c \n\n";
+	cout<<"d \n\n";
+	
+	#nosaka vai ir atbildets pareizi vai nepareizi
+	while(n == 0){
+		cout<<"atbilde: "; cin>>atbilde;
+	if(atbilde == "a"){
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "b") {
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "c") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else if(atbilde == "d") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else{
+		cout<<"\ndriks tikkai atbildet ar a,b,c vai d \n\n";
+		}
+	}
+	n=0;
+	
+	#5.Jautajums
+	cout<<"1.jautajums \n\n";
+	cout<<"atbildi a/b \n\n";
+	
+	#Atbilzu varianti
+	cout<<"a \n\n";
+	cout<<"b \n\n";
+	cout<<"c \n\n";
+	cout<<"d \n\n";
+	
+	#nosaka vai ir atbildets pareizi vai nepareizi
+	while(n == 0){
+		cout<<"atbilde: "; cin>>atbilde;
+	if(atbilde == "a"){
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "b") {
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "c") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else if(atbilde == "d") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else{
+		cout<<"\ndriks tikkai atbildet ar a,b,c vai d \n\n";
+		}
+	}
+	n=0;
+	
+	#6.Jautajums
+	cout<<"1.jautajums \n\n";
+	cout<<"atbildi a/b \n\n";
+	
+	#Atbilzu varianti
+	cout<<"a \n\n";
+	cout<<"b \n\n";
+	cout<<"c \n\n";
+	cout<<"d \n\n";
+	
+	#nosaka vai ir atbildets pareizi vai nepareizi
+	while(n == 0){
+		cout<<"atbilde: "; cin>>atbilde;
+	if(atbilde == "a"){
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "b") {
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "c") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else if(atbilde == "d") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else{
+		cout<<"\ndriks tikkai atbildet ar a,b,c vai d \n\n";
+		}
+	}
+	n=0;
+	
+	#7.Jautajums
+	cout<<"1.jautajums \n\n";
+	cout<<"atbildi a/b \n\n";
+	
+	#Atbilzu varianti
+	cout<<"a \n\n";
+	cout<<"b \n\n";
+	cout<<"c \n\n";
+	cout<<"d \n\n";
+	
+	#nosaka vai ir atbildets pareizi vai nepareizi
+	while(n == 0){
+		cout<<"atbilde: "; cin>>atbilde;
+	if(atbilde == "a"){
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "b") {
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "c") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else if(atbilde == "d") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else{
+		cout<<"\ndriks tikkai atbildet ar a,b,c vai d \n\n";
+		}
+	}
+	n=0;
+	
+	#8.Jautajums
+	cout<<"1.jautajums \n\n";
+	cout<<"atbildi a/b \n\n";
+	
+	#Atbilzu varianti
+	cout<<"a \n\n";
+	cout<<"b \n\n";
+	cout<<"c \n\n";
+	cout<<"d \n\n";
+	
+	#nosaka vai ir atbildets pareizi vai nepareizi
+	while(n == 0){
+		cout<<"atbilde: "; cin>>atbilde;
+	if(atbilde == "a"){
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "b") {
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "c") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else if(atbilde == "d") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else{
+		cout<<"\ndriks tikkai atbildet ar a,b,c vai d \n\n";
+		}
+	}
+	n=0;
+	
+	#9.Jautajums
+	cout<<"1.jautajums \n\n";
+	cout<<"atbildi a/b \n\n";
+	
+	#Atbilzu varianti
+	cout<<"a \n\n";
+	cout<<"b \n\n";
+	cout<<"c \n\n";
+	cout<<"d \n\n";
+	
+	#nosaka vai ir atbildets pareizi vai nepareizi
+	while(n == 0){
+		cout<<"atbilde: "; cin>>atbilde;
+	if(atbilde == "a"){
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "b") {
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "c") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else if(atbilde == "d") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else{
+		cout<<"\ndriks tikkai atbildet ar a,b,c vai d \n\n";
+		}
+	}
+	n=0;
+	
+	#10.Jautajums
+	cout<<"1.jautajums \n\n";
+	cout<<"atbildi a/b \n\n";
+	
+	#Atbilzu varianti
+	cout<<"a \n\n";
+	cout<<"b \n\n";
+	cout<<"c \n\n";
+	cout<<"d \n\n";
+	
+	#nosaka vai ir atbildets pareizi vai nepareizi
+	while(n == 0){
+		cout<<"atbilde: "; cin>>atbilde;
+	if(atbilde == "a"){
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "b") {
+		cout<<"pareizi";
+		n++;
+	}else if(atbilde == "c") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else if(atbilde == "d") {
+		cout<<"nepareizi";
+		n++;
+		j++;
+	}else{
+		cout<<"\ndriks tikkai atbildet ar a,b,c vai d \n\n";
+		}
+	}
+	n=0;
+	
+	
+}
