0eefd67f239e905971c1b4f3d6d3070f4bc0413d
unknown <emilsporikis2@gmail.com>
2021-06-15 13:12:08 +0300

    Salikti jautājumi

--- Eksamens.cpp
+++ Eksamens.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 int main(){
 	string atbilde;
-	int n=0, j=0, ;
+	int n=0, j=0;
 	
-	#1.Jautajums
+	//1.Jautajums
 	cout<<"1.jautajums \n\n";
-	cout<<"atbildi a/b \n\n";
+	cout<<"Kura sporta veida izmanto bumbu? \n\n";
 	
-	#Atbilzu varianti
-	cout<<"a \n\n";
-	cout<<"b \n\n";
-	cout<<"c \n\n";
-	cout<<"d \n\n";
+	//Atbilzu varianti
+	cout<<"a) Basketbols\n";
+	cout<<"b) Dailslidosana\n";
+	cout<<"c) Futbols\n";
+	cout<<"d) hokejs\n\n";
 	
-	#nosaka vai ir atbildets pareizi vai nepareizi
+	//nosaka vai ir atbildets pareizi vai nepareizi
 	while(n == 0){
 		cout<<"atbilde: "; cin>>atbilde;
 	if(atbilde == "a"){
@@ -38,17 +38,17 @@ int main(){
 	}
 	n=0;
 	
-	#2.Jautajums
-	cout<<"1.jautajums \n\n";
-	cout<<"atbildi a/b \n\n";
+	//2.Jautajums
+	cout<<"2.jautajums \n\n";
+	cout<<"Kuras grâmatas sarakstija Anna Sakse? \n\n";
 	
-	#Atbilzu varianti
-	cout<<"a \n\n";
-	cout<<"b \n\n";
-	cout<<"c \n\n";
-	cout<<"d \n\n";
+	//Atbilzu varianti
+	cout<<"a) Pasaka par ziediem\n";
+	cout<<"b) Kauja pie knipskas\n";
+	cout<<"c) Lidojums uz meness\n";
+	cout<<"d) Suna ciema Zeni\n\n";
 	
-	#nosaka vai ir atbildets pareizi vai nepareizi
+	//nosaka vai ir atbildets pareizi vai nepareizi
 	while(n == 0){
 		cout<<"atbilde: "; cin>>atbilde;
 	if(atbilde == "a"){
@@ -71,17 +71,17 @@ int main(){
 	}
 	n=0;
 	
-	#3.Jautajums
-	cout<<"1.jautajums \n\n";
-	cout<<"atbildi a/b \n\n";
+	//3.Jautajums
+	cout<<"3.jautajums 					\n\n";
+	cout<<"Kuras ir ""Zivu"" dziesmas?	\n\n";
 	
-	#Atbilzu varianti
-	cout<<"a \n\n";
-	cout<<"b \n\n";
-	cout<<"c \n\n";
-	cout<<"d \n\n";
+	//Atbilzu varianti
+	cout<<"a) Zilite				\n";
+	cout<<"b) Disnejlenda			\n";
+	cout<<"c) Cetri balti krekli	\n";
+	cout<<"d) Ziemelmeitas			\n\n";
 	
-	#nosaka vai ir atbildets pareizi vai nepareizi
+	//nosaka vai ir atbildets pareizi vai nepareizi
 	while(n == 0){
 		cout<<"atbilde: "; cin>>atbilde;
 	if(atbilde == "a"){
@@ -104,17 +104,17 @@ int main(){
 	}
 	n=0;
 	
-	#4.Jautajums
-	cout<<"1.jautajums \n\n";
-	cout<<"atbildi a/b \n\n";
+	//4.Jautajums
+	cout<<"4.jautajums \n\n";
+	cout<<"Kuri ir bijusi latvijas valsts prezidenti?\n\n";
 	
-	#Atbilzu varianti
-	cout<<"a \n\n";
-	cout<<"b \n\n";
-	cout<<"c \n\n";
-	cout<<"d \n\n";
+	//Atbilzu varianti
+	cout<<"a) Karlis Caks\n";
+	cout<<"b) Alberts kviesis\n";
+	cout<<"c) Viktors zemgalis\n";
+	cout<<"d) Imants Ziedonis\n\n";
 	
-	#nosaka vai ir atbildets pareizi vai nepareizi
+	//nosaka vai ir atbildets pareizi vai nepareizi
 	while(n == 0){
 		cout<<"atbilde: "; cin>>atbilde;
 	if(atbilde == "a"){
@@ -137,17 +137,17 @@ int main(){
 	}
 	n=0;
 	
-	#5.Jautajums
-	cout<<"1.jautajums \n\n";
-	cout<<"atbildi a/b \n\n";
+	//5.Jautajums
+	cout<<"5.jautajums \n\n";
+	cout<<"Kura sporta veida izmanto nujas? \n\n";
 	
-	#Atbilzu varianti
-	cout<<"a \n\n";
-	cout<<"b \n\n";
-	cout<<"c \n\n";
-	cout<<"d \n\n";
+	//Atbilzu varianti
+	cout<<"a) Hokejs\n";
+	cout<<"b) Polo\n";
+	cout<<"c) Regbijs\n";
+	cout<<"d) Novuss\n\n";
 	
-	#nosaka vai ir atbildets pareizi vai nepareizi
+	//nosaka vai ir atbildets pareizi vai nepareizi
 	while(n == 0){
 		cout<<"atbilde: "; cin>>atbilde;
 	if(atbilde == "a"){
@@ -170,17 +170,17 @@ int main(){
 	}
 	n=0;
 	
-	#6.Jautajums
-	cout<<"1.jautajums \n\n";
-	cout<<"atbildi a/b \n\n";
+	//6.Jautajums
+	cout<<"6.jautajums \n\n";
+	cout<<"Kuri ir Liepajas diktori? \n\n";
 	
-	#Atbilzu varianti
-	cout<<"a \n\n";
-	cout<<"b \n\n";
-	cout<<"c \n\n";
-	cout<<"d \n\n";
+	//Atbilzu varianti
+	cout<<"a) Arnis Krauze\n";
+	cout<<"b) Valters Krauze\n";
+	cout<<"c) Ansis Bogustovs\n";
+	cout<<"d) Kristaps Porzingis\n\n";
 	
-	#nosaka vai ir atbildets pareizi vai nepareizi
+	//nosaka vai ir atbildets pareizi vai nepareizi
 	while(n == 0){
 		cout<<"atbilde: "; cin>>atbilde;
 	if(atbilde == "a"){
@@ -203,17 +203,17 @@ int main(){
 	}
 	n=0;
 	
-	#7.Jautajums
-	cout<<"1.jautajums \n\n";
-	cout<<"atbildi a/b \n\n";
+	//7.Jautajums
+	cout<<"7.jautajums \n\n";
+	cout<<"Kuri ir liepajas aktieri? \n\n";
 	
-	#Atbilzu varianti
-	cout<<"a \n\n";
-	cout<<"b \n\n";
-	cout<<"c \n\n";
-	cout<<"d \n\n";
+	//Atbilzu varianti
+	cout<<"a) Rezija Kalnina\n";
+	cout<<"b) Katrina Tatarinova\n";
+	cout<<"c) Gints Gravelis\n";
+	cout<<"d) Girts Kesters\n\n";
 	
-	#nosaka vai ir atbildets pareizi vai nepareizi
+	//nosaka vai ir atbildets pareizi vai nepareizi
 	while(n == 0){
 		cout<<"atbilde: "; cin>>atbilde;
 	if(atbilde == "a"){
@@ -236,17 +236,17 @@ int main(){
 	}
 	n=0;
 	
-	#8.Jautajums
-	cout<<"1.jautajums \n\n";
-	cout<<"atbildi a/b \n\n";
+	//8.Jautajums
+	cout<<"8.jautajums \n\n";
+	cout<<"Kuras ir eiropas savienibas valstis? \n\n";
 	
-	#Atbilzu varianti
-	cout<<"a \n\n";
-	cout<<"b \n\n";
-	cout<<"c \n\n";
-	cout<<"d \n\n";
+	//Atbilzu varianti
+	cout<<"a) Italija\n";
+	cout<<"b) Gvatemala\n";
+	cout<<"c) Gana\n";
+	cout<<"d) Latvija\n\n";
 	
-	#nosaka vai ir atbildets pareizi vai nepareizi
+	//nosaka vai ir atbildets pareizi vai nepareizi
 	while(n == 0){
 		cout<<"atbilde: "; cin>>atbilde;
 	if(atbilde == "a"){
@@ -269,17 +269,17 @@ int main(){
 	}
 	n=0;
 	
-	#9.Jautajums
-	cout<<"1.jautajums \n\n";
-	cout<<"atbildi a/b \n\n";
+	//9.Jautajums
+	cout<<"9.jautajums \n\n";
+	cout<<"Kuras ir Latvijas upes? \n\n";
 	
-	#Atbilzu varianti
-	cout<<"a \n\n";
-	cout<<"b \n\n";
-	cout<<"c \n\n";
-	cout<<"d \n\n";
+	//Atbilzu varianti
+	cout<<"a) Kaulinujute\n";
+	cout<<"b) Abava\n";
+	cout<<"c) Ieniseja\n";
+	cout<<"d) Nila\n\n";
 	
-	#nosaka vai ir atbildets pareizi vai nepareizi
+	//nosaka vai ir atbildets pareizi vai nepareizi
 	while(n == 0){
 		cout<<"atbilde: "; cin>>atbilde;
 	if(atbilde == "a"){
@@ -302,17 +302,17 @@ int main(){
 	}
 	n=0;
 	
-	#10.Jautajums
-	cout<<"1.jautajums \n\n";
-	cout<<"atbildi a/b \n\n";
+	//10.Jautajums
+	cout<<"10.jautajums \n\n";
+	cout<<"Kuras Latvijas pilsetas ir tramvajs? \n\n";
 	
-	#Atbilzu varianti
-	cout<<"a \n\n";
-	cout<<"b \n\n";
-	cout<<"c \n\n";
-	cout<<"d \n\n";
+	//Atbilzu varianti
+	cout<<"a) Rezekne\n";
+	cout<<"b) Liepaja\n";
+	cout<<"c) Daugavpils\n";
+	cout<<"d) Saldus\n\n";
 	
-	#nosaka vai ir atbildets pareizi vai nepareizi
+	//nosaka vai ir atbildets pareizi vai nepareizi
 	while(n == 0){
 		cout<<"atbilde: "; cin>>atbilde;
 	if(atbilde == "a"){
